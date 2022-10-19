ffa10fbcedd95e6aea05d62400987218cb1c4684
Nothando <nk.mashabane@gmail.com>
2022-10-18 20:54:17 -0700

    Tasks

--- main.h
+++ main.h
@@ -24,5 +24,31 @@ int print_hexa(va_list types, char map_to[];
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[];
 int flags, int width, int precision, int size);
+int print_pointer(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int print_reverse(va_list types, char buffer[];
+int flags, int width, int precision, int size);
+int print_rot13string(va_list types, char buffer[];
+int flags, int width, int precision, int size);
+int handle_write_char(char c, char buffer[];
+int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[];
+int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length;
+int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsgnd(int is_negative, int ind;
+char buffer[];
+int flags, int width, int precision, int size);
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif
+#endif /*MAIN_H*/
